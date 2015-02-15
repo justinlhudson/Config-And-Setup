9b884b111fa971c4eb49d040ba0734fbd778b6ef
Justin L Hudson <justinlhudson@gmail.com>
2015-02-14 20:06:53 -0500

    boinc

--- Linux/install.sh
+++ Linux/install.sh
@@ -8,3 +8,10 @@ fi
 apt-get install silversearcher-ag expect install gdisk htop ssh vsftpd samba vim filezilla nautilus-dropbox bind9 
 # apt-get install jump 
 
+# seti/boinc cliet
+# apt-get install python-software-properties
+# add-apt-repository ppa:costamagnagianfranco/boinc
+# apt-get update
+# apt-get install boinc-client
+# /etc/default/boinc-client
+# boinccmd
