986e9ee85e1b34cd06ee9d05a2de798b5abc4dbd
Justin L Hudson <justinlhudson@gmail.com>
2015-11-26 18:32:04 -0500

    with dual ISP, want to know if designated computer ISP goes down (unlimited ISP verse not unlimited ISP)

---
+++ Linux/ISP_Checker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#ISP_Checker.sh <trace address> <hops> <check> <mail address>
+
+# Notes:
+  #apt-get install -f mailutils
+  #mail -s "Test Text" "test@SomeDomain.com,#@messaging.sprintpcs.com"
+
+_mail="$4"
+_check="$3"
+_file="/tmp/ISP-Checker-$_check"
+_result=$(traceroute $1 -m $2)
+
+echo $_result
+echo
+
+if [[ ! $_result = *$_check* ]]; then
+  echo "ISP-Down: $_check"
+  if [ ! -f $_file ]; then  
+    echo "alert!"
+    touch $_file
+    echo $_check | mail -s "ISP-Down: $_check" $_mail < /dev/null
+  fi
+else
+  echo "ISP-UP: $_check"
+  if [ -f $_file ]; then
+    echo "Alert!"
+    rm -f $_file
+    echo $_check | mail -s "ISP-Up: $_check" $_mail < /dev/null
+  fi
+fi
