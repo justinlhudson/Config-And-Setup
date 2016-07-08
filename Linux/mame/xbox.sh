aeb08c6c8904837368c1bb905a42c01a8abee7c1
Justin L. Hudson <justinlhudson@gmail.com>
2016-07-08 14:09:46 -0400

    Update xbox.sh
    added configure notes...  someday make automated script to edit files, but lazy right now

--- Linux/mame/xbox.sh
+++ Linux/mame/xbox.sh
@@ -2,6 +2,9 @@
 
 #xbox 360 controller driver setup
 # add xboxdrv to group of user running:  http://pingus.seul.org/~grumbel/xboxdrv/xboxdrv.html
+# Configure:
+# sudo visudo
+# kiosk ALL=(ALL) NOPASSWD: /usr/bin/xboxdrv
 sudo apt-add-repository -y ppa:rael-gc/ubuntu-xboxdrv
 sudo apt-get update
 sudo apt-get install ubuntu-xboxdrv
