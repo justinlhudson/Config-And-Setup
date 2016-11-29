8e7e88ef7c9af3816635e51cac51cf01dd6dd3f9
Justin L. Hudson <justinlhudson@gmail.com>
2016-11-29 10:38:40 -0500

    Update pm2-init.sh
    snippets to get pm2 working

--- Configs/etc/init.d/pm2-init.sh
+++ Configs/etc/init.d/pm2-init.sh
@@ -1,3 +1,15 @@
-# edit variables:
-USER=<user>
-export PM2_HOME="/home/<user>/.pm2"
+### Notes:
+#/etc/init.d/pm2-init.sh
+# chown <username>:<username> /home/<username>/.pm2
+# ??? chmod 755 -R /home/<username>/.pm2
+# sudo env PATH=$PATH:/usr/bin pm2 startup ubuntu -u <username>
+# pm2 start ecosystem.config.js
+# pm2 save
+
+# Edit:
+NAME=pm2
+PM2=/usr/local/bin/pm2
+USER=<username>
+DEFAULT=/etc/default/$NAME
+
+export PM2_HOME="/home/$USER/.pm2"
