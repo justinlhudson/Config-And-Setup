9954c4dafcd87ad903872ae4448b1715723ece4a
Justin L. Hudson <justinlhudson@gmail.com>
2016-12-22 13:48:02 -0500

    Update pm2-init.sh
    permission issues

--- Configs/etc/init.d/pm2-init.sh
+++ Configs/etc/init.d/pm2-init.sh
@@ -2,6 +2,8 @@
 #/etc/init.d/pm2-init.sh
 # chown <username>:<username> /home/<username>/.pm2
 # ??? chmod 755 -R /home/<username>/.pm2
+# ??? sudo chown -R root:staff /usr/local/lib/node_modules/pm2
+# ??? sudo chmod -R 775 /usr/local/lib/node_modules/pm2
 # sudo env PATH=$PATH:/usr/bin pm2 startup ubuntu -u <username>
 # pm2 start ecosystem.config.js
 # pm2 save
