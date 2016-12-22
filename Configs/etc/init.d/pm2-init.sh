1fd2c5bb7cc3af837c1a14e31c56f9ae0b5ba343
Justin L. Hudson <justinlhudson@gmail.com>
2016-12-22 13:52:01 -0500

    Update pm2-init.sh
    home directory permissions changed so gave comment on how to change

--- Configs/etc/init.d/pm2-init.sh
+++ Configs/etc/init.d/pm2-init.sh
@@ -4,6 +4,7 @@
 # ??? chmod 755 -R /home/<username>/.pm2
 # ??? sudo chown -R root:staff /usr/local/lib/node_modules/pm2
 # ??? sudo chmod -R 775 /usr/local/lib/node_modules/pm2
+# ??? ***WINNER*** sudo chown -R $USER:$USER ~/.pm2
 # sudo env PATH=$PATH:/usr/bin pm2 startup ubuntu -u <username>
 # pm2 start ecosystem.config.js
 # pm2 save
