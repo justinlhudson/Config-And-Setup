3fb0d533e232b4753e4ecddab1b10b87ce3acce9
Justin L. Hudson <justinlhudson@gmail.com>
2017-02-27 18:57:23 -0500

    Update pm2-init.sh

--- Configs/etc/init.d/pm2-init.sh
+++ Configs/etc/init.d/pm2-init.sh
@@ -1,11 +1,11 @@
 ### Notes:
 #/etc/init.d/pm2-init.sh
-# chown <username>:<username> /home/<username>/.pm2
-# ??? chmod 755 -R /home/<username>/.pm2
+# chown $USER:$USER /home/$USER/.pm2
+# ??? chmod 755 -R /home/$USER/.pm2
 # ??? sudo chown -R root:staff /usr/local/lib/node_modules/pm2
 # ??? sudo chmod -R 775 /usr/local/lib/node_modules/pm2
 # ??? ***WINNER*** sudo chown -R $USER:$USER ~/.pm2
-# sudo env PATH=$PATH:/usr/bin pm2 startup ubuntu -u <username>
+# sudo env PATH=$PATH:/usr/bin pm2 startup ubuntu -u $USER
 # pm2 start ecosystem.config.js
 # pm2 save
 
