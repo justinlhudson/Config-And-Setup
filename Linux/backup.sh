eaf088afc6a52a1945c5910e757cbbc59dd52d79
Justin L. Hudson <justinlhudson@gmail.com>
2017-10-23 14:41:55 -0400

    Update backup.sh
    tested

--- Linux/backup.sh
+++ Linux/backup.sh
@@ -49,7 +49,7 @@ sudo sh -c "tar --exclude="$_directory" --exclude='*.ecryptfs/*' --exclude='/tmp
 ### Restore:  tar xvfpz 
 
 # Mongodb backup
-if type "mongodump" > /dev/null; then
+if type mongodump > /dev/null; then
   mongodump --archive=mongodb_"$_mark".gz --gzip
   # double history, since have two files now
   _history=$((2 * $_history))
