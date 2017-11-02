f326e6011d94d0e53285e564fd47d37e80e86508
Justin L. Hudson <justinlhudson@gmail.com>
2017-11-02 13:18:34 -0400

    Update env.cmd

--- Windows/env.cmd
+++ Windows/env.cmd
@@ -1,7 +1,8 @@
 @echo off
 
-:: Add to target shorcut foc 'cmd'
+:: Add to target shorcut for 'cmd'
 :: cmd.exe /K C:\path\to\env.cmd
 
 :: Aliases
 doskey clear=cls
+doskey python=C:\<path>\anaconda\4.2.0\python
