46b6e28ef711d377aa383c73fae6f6556b662877
Justin L. Hudson <justinlhudson@gmail.com>
2022-10-25 20:59:03 -0400

    Delete Windows directory

--- Windows/MouseInvert.ps1
+++
@@ -1,8 +0,0 @@
-### Invert Mouse like OSX ###
-
-# Powershell:
-Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
-
-# regedit.exe
-# HKEY_LOCAL_MACHINE/SYSTEM/CurrentControlSet/Enum/HID/<Hardware ID>/Device Parameters
-# FlipFlopWheel from 0 to 1
--- Windows/env.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: Add to target shorcut for 'cmd'
-:: cmd.exe /K C:\path\to\env.cmd
-
-:: Aliases
-doskey clear=cls
-doskey python=C:\<path>\anaconda\4.2.0\python $*
